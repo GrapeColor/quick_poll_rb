09fb7eb1cfb028fc177856be800d041dce096ed1
GrapeColor <grapecolor@users.noreply.github.com>
2020-03-03 18:20:12 +0900

    Update admin command

--- quick_poll.rb
+++ quick_poll.rb
@@ -102,14 +102,22 @@ class QuickPoll
 
     # デバッグコマンド
     @bot.mention(in: ENV['ADMIN_CHANNEL_ID'].to_i, from: ENV['ADMIN_USER_ID'].to_i) do |event|
-      next if event.content !~ /^<@!?\d+> admin (.+)/
+      next if event.content !~ /^<@!?\d+>\s+admin\R```(ruby)?\R(.+)\R```/m
+
+      $stdout = StringIO.new
 
       begin
-        value = eval($1)
+        eval("pp(#{$2})")
+        log = $stdout.string
       rescue => exception
-        value = exception
+        log = exception
       end
-      event << "```\n#{value}\n```"
+
+      $stdout = STDOUT
+
+      log.to_s.scan(/.{1,#{2000 - 8}}/m) do |split|
+        event.send_message("```\n#{split}\n```")
+      end
     end
   end
 
