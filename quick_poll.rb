40c9bd3874c54399f5d3594f6c09d0d2fadf5120
GrapeColor <grapecolor@users.noreply.github.com>
2020-04-28 20:42:22 +0900

    Deleted other commands

--- quick_poll.rb
+++ quick_poll.rb
@@ -6,7 +6,6 @@ require 'discordrb'
 require_relative './poll_commands'
 require_relative './result_command'
 require_relative './help_command'
-require_relative './other_commands'
 require_relative './admin_command'
 
 class QuickPoll
@@ -30,7 +29,6 @@ class QuickPoll
 
     set_poll_commands
     set_result_command
-    set_other_commands
     set_admin_command
   end
 
