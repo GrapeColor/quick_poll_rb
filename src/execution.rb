aa3ccd0b153c07a7b662dca2d573727e2c83f22f
GrapeColor <grapecolor@users.noreply.github.com>
2020-05-07 19:13:01 +0900

    Change require path

--- src/execution.rb
+++ src/execution.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'dotenv/load'
-require_relative './bot'
+require_relative './quick_poll/bot'
 
 quick_poll = QuickPoll::Bot.new(ENV['QUICK_POLL_TOKEN'])
 quick_poll.run
